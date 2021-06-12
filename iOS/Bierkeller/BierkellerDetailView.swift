0767a413c45dbb3a3e3dd93c6718277c179b5b7b
CHaenssel <wi19b093@technikum-wien.at>
2021-06-12 15:02:21 +0200

    Return to last Stable Version

--- iOS/Bierkeller/BierkellerDetailView.swift
+++ iOS/Bierkeller/BierkellerDetailView.swift
@@ -11,61 +11,60 @@ struct BierkellerDetailView: View {
     @State var name = ""
     @State var address = ""
     var body: some View {
-        NavigationView{
-            VStack{
-                VStack(alignment: .leading){
-                    Text("Name")
-                        .padding(.top, 30.0)
-                        .frame(height: 30.0)
-                    TextField("Name", text: $name)
-                        .frame(height: 40.0)
-                        .border(Color.gray)
-                    Text("Adresse")
-                        .padding(.top, 30.0)
-                        .frame(height: 30.0)
-                    TextField("Adresse", text: $address)
-                        .frame(height: 40.0)
-                        .border(Color.gray)
-                    
-                    NavigationLink(destination: ManagementDetailView()) {
-                        Image("AddBeer")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 200.0, height: 40.0)
-                            .cornerRadius(10)
-                            
-                    }
+        VStack{
+            VStack(alignment: .leading){
+                Text("Name")
                     .padding(.top, 30.0)
-                    HStack{
-                        Text("Biersorten")
-                        Spacer()
-                        Text("Menge")
-                    }
-                    .padding([.top, .leading, .trailing])
-                    Divider()
-                    List{
-                        HStack{
-                            Text("Biersorte 1")
-                            Spacer()
-                            Text("10000")
-                        }
-                        HStack{
-                            Text("Biersorte 2")
-                            Spacer()
-                            Text("10000")
-                        }
-                    }
-                    .listStyle(PlainListStyle())
-                    
+                    .frame(height: 30.0)
+                TextField("Name", text: $name)
+                    .frame(height: 40.0)
+                    .border(Color.gray)
+                Text("Adresse")
+                    .padding(.top, 30.0)
+                    .frame(height: 30.0)
+                TextField("Adresse", text: $address)
+                    .frame(height: 40.0)
+                    .border(Color.gray)
+                
+                NavigationLink(destination: ManagementDetailView()) {
+                    Image("AddBeer")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200.0, height: 40.0)
+                        .cornerRadius(10)
+                        
                 }
-                Spacer()
+                .padding(.top, 30.0)
+                HStack{
+                    Text("Biersorten")
+                    Spacer()
+                    Text("Menge")
+                }
+                .padding([.top, .leading, .trailing])
+                Divider()
+                List{
+                    HStack{
+                        Text("Biersorte 1")
+                        Spacer()
+                        Text("10000")
+                    }
+                    HStack{
+                        Text("Biersorte 2")
+                        Spacer()
+                        Text("10000")
+                    }
+                }
+                .listStyle(PlainListStyle())
                 
             }
-            .navigationBarTitle("Bierkeller Details")
-            .navigationBarTitleDisplayMode(.inline)
-            .padding()
+            Spacer()
+            
         }
+        .navigationBarTitle("Bierkeller Details")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding()
     }
+
 }
 
 struct BierkellerDetailView_Previews: PreviewProvider {
