151776a0c673708d0c877c743d5f7dd43f6221ce
wi19b101 <wi19b101@technikum-wien.at>
2021-06-22 16:20:13 +0200

    stable version

--- iOS/BeerCellar.swift
+++ iOS/BeerCellar.swift
@@ -22,6 +22,7 @@ class BeerCellar: Identifiable, Codable, ObservableObject {
         case address
     }
 
+    /*
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(Int.self, forKey: .id)
@@ -32,6 +33,7 @@ class BeerCellar: Identifiable, Codable, ObservableObject {
         let addressContainer = try container.nestedContainer(keyedBy: Address.CodingKeys.self, forKey: .address)
         address = try addressContainer.decode(Address.self, forKey: .address)
     }
+    */
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
